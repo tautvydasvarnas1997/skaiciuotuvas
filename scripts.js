faa1e03fe82d15411f01c7bc1f299ca1401c32ef
tautvydasvarnas1997 <tautvydas.varnas.97@gmail.com>
2024-12-11 09:21:27 +0200

    Added action buttons x, /, + and -

--- scripts.js
+++ scripts.js
@@ -1,5 +1,15 @@
+let firstNumber = 0;
+let secondNumber = 0;
+let action = '+';
+let answer = 0;
+
 let input = document.getElementById('calc-input');
 
 function onNumberClick(number) {
     input.value += number;
+}
+
+function onActionClick(clickedAction) {
+    input.value += ' ' + clickedAction + ' ';
+    action = clickedAction;
 }
