ddda5390aae8350b340aeffdd9ad7ba46d716f86
tautvydasvarnas1997 <tautvydas.varnas.97@gmail.com>
2024-12-11 09:34:34 +0200

    Added = button script to work with calculations

--- scripts.js
+++ scripts.js
@@ -13,3 +13,22 @@ function onActionClick(clickedAction) {
     input.value += ' ' + clickedAction + ' ';
     action = clickedAction;
 }
+
+function onCountClick() {
+    let splitted = input.value.split(' ');
+    firstNumber = parseInt(splitted[0]);
+    action = splitted[1];
+    secondNumber = parseInt(splitted[2]);
+
+    calculateAnswer();
+    input.value = answer;
+}
+
+function calculateAnswer() {
+    switch (action) {
+        case '+': answer = firstNumber + secondNumber; break;
+        case '-': answer = firstNumber - secondNumber; break;
+        case 'x': answer = firstNumber * secondNumber; break;
+        case '/': answer = firstNumber / secondNumber; break;
+    }
+}
